24396e3249b76323e499857e5f38ed482796db4e
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2018-09-27 13:51:24 +0200

    fix

--- make_dataset_helper.py
+++ make_dataset_helper.py
@@ -1,3 +1,8 @@
+import numpy as np 
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+
 def prepare_dataset():
     iris = load_iris()
     X, y = iris.data[:100,:2], iris.target[:100]
@@ -6,3 +11,4 @@ def prepare_dataset():
     X_test = X_test.T
     y_train = y_train.reshape(1, y_train.shape[0])
     y_test = y_test.reshape(1, y_test.shape[0])
+    return X_train, X_test, y_train, y_test
