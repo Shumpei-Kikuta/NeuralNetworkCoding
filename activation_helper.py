3cf4f25dbc24a52598253227b6ae1e11da1d223e
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2018-09-28 17:58:51 +0200

    delete tanh from the file

--- activation_helper.py
+++ activation_helper.py
@@ -5,11 +5,6 @@ def sigmoid(x):
     return np.where(x > 0, 1. / (1. + np.exp(-x)), np.exp(x) / (np.exp(x) + np.exp(0)))
 
 
-def tanh(x):
-    h = 0.0001
-    return (np.exp(x + h) - np.exp(-x + h)) / ( np.exp(x + h) + np.exp(-x + h))
-
-
 def relu(x):
     return np.maximum(0, x)
 
