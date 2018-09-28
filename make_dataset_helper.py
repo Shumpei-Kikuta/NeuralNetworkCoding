03e85645b05adbf5dc620a4a94af5fed96d006d6
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2018-09-28 12:17:17 +0200

    add figure showing the transformation of cost function

--- make_dataset_helper.py
+++ make_dataset_helper.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 
 def prepare_dataset():
     iris = load_iris()
-    X, y = iris.data[:100,:2], iris.target[:100]
+    X, y = iris.data[:100], iris.target[:100]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
     X_train = X_train.T
     X_test = X_test.T
