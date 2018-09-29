1705f11725753f6840c1d69de3f2a7be918c693a
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2018-09-29 20:52:23 +0200

    change the contents of function for multi classification

--- make_dataset_helper.py
+++ make_dataset_helper.py
@@ -9,6 +9,7 @@ def prepare_dataset():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
     X_train = X_train.T
     X_test = X_test.T
-    y_train = y_train.reshape(1, y_train.shape[0])
-    y_test = y_test.reshape(1, y_test.shape[0])
-    return X_train, X_test, y_train, y_test
+    n_labels = len(np.unique(y))
+    y_train = np.eye(n_labels)[y_train].T
+    y_test = np.eye(n_labels)[y_test].T
+    return X_train, X_test, y_train, y_test, n_labels
