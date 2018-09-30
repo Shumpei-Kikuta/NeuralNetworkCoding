457dbf17c9b5ac8e855e3dfb19b452ee942d2774
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2018-09-30 09:22:19 +0200

    add digits dataset

--- make_dataset_helper.py
+++ make_dataset_helper.py
@@ -1,11 +1,12 @@
 import numpy as np 
 from sklearn.datasets import load_iris
+from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 
 
-def prepare_dataset():
+def prepare_iris():
     iris = load_iris()
-    X, y = iris.data[:100], iris.target[:100]
+    X, y = iris.data, iris.target
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
     X_train = X_train.T
     X_test = X_test.T
@@ -13,3 +14,16 @@ def prepare_dataset():
     y_train = np.eye(n_labels)[y_train].T
     y_test = np.eye(n_labels)[y_test].T
     return X_train, X_test, y_train, y_test, n_labels
+
+
+def prepare_digits():
+    digit = load_digits()
+    X, y = digit.data, digit.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+    X_train = X_train.T
+    X_test = X_test.T
+    n_labels = len(np.unique(y))
+    y_train = np.eye(n_labels)[y_train].T
+    y_test = np.eye(n_labels)[y_test].T
+    return X_train, X_test, y_train, y_test, n_labels
+    
