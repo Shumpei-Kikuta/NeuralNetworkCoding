2ae2546afdbc4a3a94d04e5c4707eb792d12bfd4
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2018-10-01 09:23:25 +0200

    add mnist dataset

--- make_dataset_helper.py
+++ make_dataset_helper.py
@@ -1,7 +1,16 @@
+"""
+Create the dataset 
+
+Input: None
+Output: X_train, X_test, y_train, y_test, n_labels
+"""
+
 import numpy as np 
 from sklearn.datasets import load_iris
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras.datasets.mnist import load_data
 
 
 def prepare_iris():
@@ -25,5 +34,17 @@ def prepare_digits():
     n_labels = len(np.unique(y))
     y_train = np.eye(n_labels)[y_train].T
     y_test = np.eye(n_labels)[y_test].T
+    return X_train, X_test, y_train, y_test, n_labels
+
+
+
+def prepare_mnist():
+    (pre_X_train, pre_y_train), (pre_X_test, pre_y_test) = load_data()
+    X_train = pre_X_train.reshape(pre_X_train.shape[0], pre_X_train.shape[1] * pre_X_train.shape[2]).T
+    X_test = pre_X_test.reshape(pre_X_test.shape[0], pre_X_test.shape[1] * pre_X_test.shape[2]).T
+    n_labels = len(np.unique(pre_y_train))
+    y_train = np.eye(n_labels)[pre_y_train].T
+    y_test = np.eye(n_labels)[pre_y_test].T
+
     return X_train, X_test, y_train, y_test, n_labels
     
